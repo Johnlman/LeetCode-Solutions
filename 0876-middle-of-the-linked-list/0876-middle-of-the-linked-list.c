5e3b1fc1fb43ffdf62594cf171ea60c902fd1ce2
Johnlman <120991409+Johnlman@users.noreply.github.com>
2023-08-25 00:33:22 -0700

    Time: 0 ms (100.00%), Space: 5.9 MB (16.04%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.c
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.c
@@ -8,15 +8,9 @@
 struct ListNode* middleNode(struct ListNode* head){
     struct ListNode* slow = head;
     struct ListNode* fast= head;
-    int count = 1;
     while(fast != NULL && fast->next != NULL) {
-        if (fast->next == NULL){
-            count++;
-        } else {
-            count+=2;
-        }
         slow = slow->next;
         fast = fast->next->next;
     }
-    return count%2 == 1?slow:slow->next;
+    return slow;
 }
