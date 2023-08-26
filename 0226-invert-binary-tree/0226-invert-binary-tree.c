535e5a048bf7d8d258268563a35546b06937b096
Johnlman <120991409+Johnlman@users.noreply.github.com>
2023-08-25 20:52:44 -0700

    Time: 3 ms (47.29%), Space: 6 MB (32.19%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.c
+++ 0226-invert-binary-tree/0226-invert-binary-tree.c
@@ -6,23 +6,15 @@
  *     struct TreeNode *right;
  * };
  */
-struct TreeNode* invert(struct TreeNode* root) {
-    if (root == NULL) {
-        return NULL;
-    }
-    if (root->left == NULL && root->right == NULL) {
-        return root;
-    }
-    struct TreeNode* temp = root->left;
-    root->left = invert(root->right);
-    root->right = invert(temp);
-    return root;
-}
+
 struct TreeNode* invertTree(struct TreeNode* root){
     if (root==NULL){
         return NULL;
     }
-    return invert(root);
+    struct TreeNode* temp = root->left;
+    root->left = invertTree(root->right);
+    root->right = invertTree(temp);
+    return root;
 }   
 
 
