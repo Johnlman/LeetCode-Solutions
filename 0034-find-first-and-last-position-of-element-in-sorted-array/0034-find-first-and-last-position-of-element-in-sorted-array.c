1f55d3a2d3d33dea67656a2359b2b38d2a04dd5e
Johnlman <120991409+Johnlman@users.noreply.github.com>
2023-08-31 21:46:02 -0700

    Time: 9 ms (55.28%), Space: 7.4 MB (67.97%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.c
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.c
@@ -6,13 +6,28 @@ int* searchRange(int* nums, int numsSize, int target, int* returnSize){
     int* ret = malloc(sizeof(int)*2);
     ret[0] = -1;
     ret[1] = -1;
-    for (int i = 0;i<numsSize;i++){
-        if (ret[0] == -1 && nums[i] == target){
-            ret[0] =i;
-            ret[1] =i;
-        } else if (ret[0] != -1 && nums[i] == target){
-            ret[1] = i;
-        }
+    int l = 0;
+    int r = numsSize-1;
+    while (l<=r){
+        int m = l + (r - l) / 2;
+        if (nums[m] == target){
+            int left = m;
+            int right = m;
+            while(left>= 0 && nums[left]==target){
+                left--;
+            }
+            ret[0] = ++left;
+            while(right< numsSize &&nums[right]==target){
+                right++;
+            }
+            ret[1] = --right;
+            return ret;
+         } else if (nums[m] < target){
+            l = m + 1;
+        } else {
+            r = m - 1;
+        } 
     }
     return ret;
-}
+    }
+    
