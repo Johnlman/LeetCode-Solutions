a7b87bc8dd37df34f5bb23b348b413a7dfccf450
Johnlman <120991409+Johnlman@users.noreply.github.com>
2023-09-01 20:08:40 -0700

    Time: 22 ms (68.95%), Space: 8.9 MB (97.06%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.c
+++ 0200-number-of-islands/0200-number-of-islands.c
@@ -1,21 +1,3 @@
-void visit(char** grid, int gridSize, int* gridColSize,int i,int j);
-
-int numIslands(char** grid, int gridSize, int* gridColSize){
-
-    int ret = 0;
-    for (int i = 0;i<gridSize;i++){
-        for(int j = 0;j<gridColSize[i];j++){
-            if (grid[i][j]=='1'){
-                ret++;
-                visit(grid,gridSize,gridColSize,i,j);
-            }
-            
-        }
-    }
-    return ret;
-}
-
-
 void visit(char** grid, int gridSize, int* gridColSize,int i,int j){
     if ((i>=0&&i<gridSize)&&(j>=0&&j<gridColSize[0])) {
         if (grid[i][j]=='1'){
@@ -27,3 +9,19 @@ void visit(char** grid, int gridSize, int* gridColSize,int i,int j){
     }
     }
 }
+
+int numIslands(char** grid, int gridSize, int* gridColSize){
+
+    int ret = 0;
+    for (int i = 0;i<gridSize;i++){
+        for(int j = 0;j<gridColSize[i];j++){
+            if (grid[i][j]=='1'){
+                ret++;
+                visit(grid,gridSize,gridColSize,i,j);
+            }
+        }
+    }
+    return ret;
+}
+
+
