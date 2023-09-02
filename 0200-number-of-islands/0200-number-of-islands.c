dcd90909913f3d1827ec4772a806eb499c8a78d0
Johnlman <120991409+Johnlman@users.noreply.github.com>
2023-09-01 20:08:25 -0700

    Time: 18 ms (89.54%), Space: 9.2 MB (37.58%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.c
+++ 0200-number-of-islands/0200-number-of-islands.c
@@ -1,20 +1,13 @@
-void visit(char** grid, int gridSize, int* gridColSize,int** visited,int i,int j);
+void visit(char** grid, int gridSize, int* gridColSize,int i,int j);
 
 int numIslands(char** grid, int gridSize, int* gridColSize){
-    int** visited = malloc(sizeof(int*)*gridSize);
-    for (int i = 0; i < gridSize; i++) {
-        visited[i] = malloc(sizeof(int)*gridColSize[0]);
-        for (int j = 0; j < gridColSize[0]; j++) {
-            visited[i][j] = 0;
-        }
-    }
 
     int ret = 0;
     for (int i = 0;i<gridSize;i++){
         for(int j = 0;j<gridColSize[i];j++){
-            if (grid[i][j]=='1' && visited[i][j]==0){
+            if (grid[i][j]=='1'){
                 ret++;
-                visit(grid,gridSize,gridColSize,visited,i,j);
+                visit(grid,gridSize,gridColSize,i,j);
             }
             
         }
@@ -23,14 +16,14 @@ int numIslands(char** grid, int gridSize, int* gridColSize){
 }
 
 
-void visit(char** grid, int gridSize, int* gridColSize,int** visited,int i,int j){
+void visit(char** grid, int gridSize, int* gridColSize,int i,int j){
     if ((i>=0&&i<gridSize)&&(j>=0&&j<gridColSize[0])) {
-        if (!visited[i][j] && grid[i][j]=='1'){
-        visited[i][j]=1;
-        visit(grid,gridSize,gridColSize,visited,i+1,j);
-        visit(grid,gridSize,gridColSize,visited,i-1,j);
-        visit(grid,gridSize,gridColSize,visited,i,j+1);
-        visit(grid,gridSize,gridColSize,visited,i,j-1);
+        if (grid[i][j]=='1'){
+        grid[i][j]=0;
+        visit(grid,gridSize,gridColSize,i+1,j);
+        visit(grid,gridSize,gridColSize,i-1,j);
+        visit(grid,gridSize,gridColSize,i,j+1);
+        visit(grid,gridSize,gridColSize,i,j-1);
     }
     }
 }
